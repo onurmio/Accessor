4238f02970de91cf9ea68b71d7bfa53e52598fed
Onur MIO <onurmio98@gmail.com>
2021-06-22 19:26:18 +0300

    new functions added.

--- lib/accessor.dart
+++ lib/accessor.dart
@@ -8,21 +8,45 @@ class Accessor<T> {
 
   Accessor(String key) : _key = key;
 
+  void remove() {
+    _checkData(_key, _data.remove(_key));
+  }
+
+  static bool exists(String key) {
+    return _data.containsKey(key);
+  }
+
+  static Type getType(String key) {
+    return _checkKey(key, () => _data[key].runtimeType);
+  }
+
+  static bool checkType(String key, Type type) {
+    return getType(key) == type;
+  }
+
+  static _checkKey(String key, Function function) {
+    if (_data.containsKey(key))
+      return function();
+    else
+      throw AccessorException("Invalid key!");
+  }
+
+  static _checkData(String key, Function function) {
+    _checkKey(key, () {
+      if (_data[key] != null)
+        return function();
+      else
+        throw AccessorException("Data does not exist!");
+    });
+  }
+
+  bool get isEmpty => _checkKey(_key, () => _data[_key] == null);
+
   set value(T variable) {
     _data[_key] = variable;
   }
 
   T get value {
-    if (_data.containsKey(_key))
-      return _data[_key];
-    else
-      throw AccessorException("Invalid key!");
-  }
-
-  remove() {
-    if (_data.containsKey(_key))
-      _data.remove(_key);
-    else
-      throw AccessorException("Invalid key!");
+    return _checkData(_key, _data[_key]);
   }
 }
