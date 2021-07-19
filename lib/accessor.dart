e92a5857a4d2f5c00d1e311c1e7c4e006a9c37cc
Onur MIO <onurmio98@gmail.com>
2021-07-19 18:55:16 +0300

    Map type changed, listen functionality added.

--- lib/accessor.dart
+++ lib/accessor.dart
@@ -1,32 +1,39 @@
 library accessor;
 
+import 'dart:async';
+import 'dart:collection';
+
 import 'package:accessor/accessorException.dart';
 
 class Accessor<T> {
-  static Map<String, dynamic> _data = {};
+  static SplayTreeMap<String, _AccessorItem> _data = SplayTreeMap();
   String _key;
 
   Accessor(String key) : _key = key;
 
-  void remove() {
-    _checkKey(_key, () => _data.remove(_key));
-  }
+  void remove() => _checkKey(_key, () {
+        _data[_key]!.data = null;
+        _data[_key]!.notify();
+        _data.remove(_key);
+      });
 
-  static void removeAll(){
+  void listen(Function(dynamic) listener) =>
+      _checkKey(_key, () => _data[_key]!.addListener(listener));
+
+  static void removeAll() {
+    _data.forEach((key, value) {
+      value.data = null;
+      value.notify();
+    });
     _data.clear();
   }
 
-  static bool exists(String key) {
-    return _data.containsKey(key);
-  }
+  static bool exists(String key) => _data.containsKey(key);
 
-  static Type getType(String key) {
-    return _checkKey(key, () => _data[key].runtimeType);
-  }
+  static Type getType(String key) =>
+      _checkKey(key, () => _data[key]!.data.runtimeType);
 
-  static bool checkType(String key, Type type) {
-    return getType(key) == type;
-  }
+  static bool checkType(String key, Type type) => getType(key) == type;
 
   static _checkKey(String key, Function function) {
     if (_data.containsKey(key))
@@ -42,9 +49,24 @@ class Accessor<T> {
       if (!checkType(_key, T))
         throw AccessorException(
             "type '${variable.runtimeType}' is not a subtype of type '${getType(_key)}'");
+      _data[_key]!.data = variable;
+      _data[_key]!.notify();
+      return;
     }
-    _data[_key] = variable;
+    _data[_key] = _AccessorItem(variable);
   }
 
-  T get value => _checkKey(_key, () => _data[_key]);
+  T get value => _checkKey(_key, () => _data[_key]!.data);
+}
+
+class _AccessorItem {
+  StreamController streamController = StreamController.broadcast();
+  var data;
+
+  _AccessorItem(data) : data = data;
+
+  void addListener(Function(dynamic) listener) =>
+      streamController.stream.listen(listener);
+
+  void notify() => streamController.add(data);
 }
